1c16985e55792e9bbde77fc6d9272851f5de92fa
JahirMM <jahirmachuca45@gmail.com>
2025-01-04 18:58:15 -0300

    feat(SalesSection): finish the styles

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/home/components/hero/Hero";
 import RecentSection from "@/home/components/recentSection/RecentSection";
+import SalesSection from "@/home/salesSection/SalesSection";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <RecentSection />
+      <SalesSection />
     </>
   );
 }
---
+++ src/home/salesSection/SalesSection.tsx
@@ -0,0 +1,36 @@
+import ArrowIcon from "@/icons/ArrowIcon";
+import ProductItem from "@/share/components/ProductItem";
+
+function SalesSection() {
+  return (
+    <section className="p-10 bg-white flex flex-col gap-5 md:flex-row md:items-center">
+      <header className="md:w-[50%]">
+        <h2 className="text-lg font-bold">Ofertas</h2>
+        <p className="mb-5 mt-5 text-sm text-pretty">
+          Descuentos increibles en tecnología. No pierdas la oportunidad de
+          llevarte lo mejor en tecnología a precios inigualables
+        </p>
+        <button className="bg-primaryColor flex items-center gap-5 px-3 py-1 rounded-xl">
+          <span className="text-sm text-white">Ver todos</span>
+          <ArrowIcon className="size-4 text-white" />
+        </button>
+      </header>
+      <ul
+        className="w-full overflow-auto flex gap-5"
+        aria-label="Lista de productos recientes"
+      >
+        <li>
+          <ProductItem />
+        </li>
+        <li>
+          <ProductItem />
+        </li>
+        <li>
+          <ProductItem />
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default SalesSection;
