b24d2ad120381580bd4588b5e436389eb7081b04
JahirMM <jahirmachuca45@gmail.com>
2025-01-25 17:38:52 -0300

    style(ProfileNav): complete the design

--- src/profile/components/ProfileNav.tsx
+++ src/profile/components/ProfileNav.tsx
@@ -1,61 +1,55 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 import Link from "next/link";
 
 import BagShoppingIcon from "@/icons/BagShoppingIcon";
 import UserCircleIcon from "@/icons/UserCircleIcon";
 import ChevronIcon from "@/icons/ChevronIcon";
-import BoxIcon from "@/icons/BoxIcon";
 import SaleIcon from "@/icons/SaleIcon";
+import BoxIcon from "@/icons/BoxIcon";
+
+const menuItems = [
+  { url: "/profile", label: "Mi perfil", Icon: UserCircleIcon },
+  { url: "/profile/purchases", label: "Compras", Icon: BagShoppingIcon },
+  { url: "/profile/sales", label: "Ventas", Icon: SaleIcon },
+  { url: "/profile/my-products", label: "Mis productos", Icon: BoxIcon },
+];
 
 function ProfileNav() {
   const [showMenu, setShowMenu] = useState(false);
+  const pathname = usePathname();
 
   const toggleMenu = () => {
     setShowMenu((prev) => !prev);
   };
   return (
-    <section className="absolute z-10 w-full px-3 py-4 mt-5 bg-secondaryColor sm:rounded-xl sm:py-5 sm:relative sm:w-auto">
+    <section className="absolute z-10 w-full px-3 py-4 mt-5 bg-secondaryColor sm:rounded-xl sm:py-5 sm:px-0 sm:relative">
       <div
-        className={`flex items-center gap-x-2 ${showMenu ? "mb-4" : "mb-0"} cursor-pointer sm:mb-10 sm:pointer-events-none`}
+        className={`flex items-center gap-x-2 ${
+          showMenu ? "mb-4" : "mb-0"
+        } px-3 cursor-pointer sm:mb-10 sm:pointer-events-none`}
         onClick={toggleMenu}
       >
-        <span className="text-sm">Mi cuenta</span>
-        <ChevronIcon className="size-3" />
+        <span className="font-bold text-md">Mi cuenta</span>
+        <ChevronIcon className={`size-3 sm:hidden transition-transform duration-700 ease-linear ${showMenu ? "rotate-90" : " rotate-0"}`} />
       </div>
-      <nav className={`${showMenu ? "inline-block" : "hidden"} sm:inline-block`}>
-        <ul className="flex flex-col gap-y-8">
-          <li>
-            <Link href={"/profile"} className="flex items-center gap-x-3">
-              <UserCircleIcon className="size-7" />
-              <span className="text-sm">Mi perfil</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={"/profile/purchases"}
-              className="flex items-center gap-x-3"
-            >
-              <BagShoppingIcon className="size-7" />
-              <span className="text-sm">Compras</span>
-            </Link>
-          </li>
-          <li>
-            <Link href={"/profile/sales"} className="flex items-center gap-x-3">
-              <SaleIcon className="size-7" />
-              <span className="text-sm">Ventas</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={"/profile/my-products"}
-              className="flex items-center gap-x-3"
-            >
-              <BoxIcon className="size-7" />
-              <span className="text-sm">Mis productos</span>
-            </Link>
-          </li>
+      <nav
+        className={`${
+          showMenu ? "inline-block" : "hidden"
+        } sm:inline-block sm:w-full`}
+      >
+        <ul className="flex flex-col w-full gap-y-8">
+          {menuItems.map(({ url, label, Icon }, index) => (
+            <li key={label} className="flex px-3 group sm:px-0">
+              <span className="hidden w-1 mr-2 sm:block group-hover:bg-primaryColor"></span>
+              <Link href={url} className="flex items-center gap-x-3">
+                <Icon className={`size-7 ${pathname === url ? "text-gray-900" : "text-gray-600"}`} />
+                <span className={`text-sm ${pathname === url ? "font-bold" : "font-normal"}`}>{label}</span>
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </section>
